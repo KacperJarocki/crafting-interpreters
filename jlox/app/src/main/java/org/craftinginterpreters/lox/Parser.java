623665c4cc5a84d7f91980fecf014ca4326a50b2
Kacper Jarocki <dev.kacperjarocki@proton.me>
2025-02-20 11:05:33 +0100

    functions, changed directory name to jlox

--- jlox/app/src/main/java/org/craftinginterpreters/lox/Parser.java
+++ jlox/app/src/main/java/org/craftinginterpreters/lox/Parser.java
@@ -25,6 +25,8 @@ class Parser {
 
   private Stmt declaration() {
     try {
+      if (match(TokenType.FUN))
+        return function("function");
       if (match(TokenType.VAR))
         return varDeclaration();
       return statement();
@@ -32,6 +34,26 @@ class Parser {
       synchronize();
       return null;
     }
+  }
+
+  private Stmt.Function function(String kind) {
+    Token name = consume(TokenType.IDENTIFIER, "Expect" + kind + " name. ");
+    consume(TokenType.LEFT_PAREN, "Expect '(' after " + kind + " name.");
+    List<Token> parameters = new ArrayList<>();
+    if (!check(TokenType.RIGHT_PAREN)) {
+      do {
+        if (parameters.size() >= 255) {
+          error(peek(), "Can't have more than 255 parameters.");
+        }
+        parameters.add(
+            consume(TokenType.IDENTIFIER, "Expect parameter name."));
+      } while (match(TokenType.COMMA));
+    }
+    consume(TokenType.RIGHT_PAREN, "Expect ')' after parameters.");
+    consume(TokenType.LEFT_BRACE, "Expect '{' before " + kind + " body.");
+    List<Stmt> body = block();
+    return new Stmt.Function(name, parameters, body);
+
   }
 
   private Stmt varDeclaration() {
@@ -53,6 +75,8 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(TokenType.RETURN))
+      return returnStatement();
     if (match(TokenType.FOR))
       return forStatement();
     if (match(TokenType.IF))
@@ -111,6 +135,16 @@ class Parser {
       elseBranch = statement();
     }
     return new Stmt.If(condition, thenBranch, elseBranch);
+  }
+
+  private Stmt returnStatement() {
+    Token keyword = previous();
+    Expr value = null;
+    if (!check(TokenType.SEMICOLON)) {
+      value = expression();
+    }
+    consume(TokenType.SEMICOLON, "Expect ';' after return value.");
+    return new Stmt.Return(keyword, value);
   }
 
   private Stmt printStatement() {
@@ -218,7 +252,33 @@ class Parser {
       Expr right = unary();
       return new Expr.Unary(operator, right);
     }
-    return primary();
+    return call();
+  }
+
+  private Expr call() {
+    Expr expr = primary();
+    while (true) {
+      if (match(TokenType.LEFT_PAREN)) {
+        expr = finishCall(expr);
+      } else {
+        break;
+      }
+    }
+    return expr;
+  }
+
+  private Expr finishCall(Expr callee) {
+    List<Expr> arguments = new ArrayList<>();
+    if (!check(TokenType.RIGHT_PAREN)) {
+      do {
+        if (arguments.size() >= 255) {
+          error(peek(), "Can't have more than 255 arguments");
+        }
+        arguments.add(expression());
+      } while (match(TokenType.COMMA));
+    }
+    Token paren = consume(TokenType.RIGHT_PAREN, "Expect ')' after arguments");
+    return new Expr.Call(callee, paren, arguments);
   }
 
   private Expr primary() {
